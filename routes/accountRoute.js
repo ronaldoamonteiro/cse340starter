b66f665194fb3d580eaf14a9a653affa7e87410a
ronaldoamonteiro <ron.monteiro07@gmail.com>
2023-11-23 13:59:37 -0300

    feat: w04 learning-activities parts 01 and 02

---
+++ routes/accountRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = new express.Router();
+const utilities = require("../utilities/");
+const accountController = require('../controllers/accountController.js');
+
+
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,14 +11,43 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute.js")
+const inventoryRoute = require("./routes/inventoryRoute.js");
+const accountRoute = require("./routes/accountRoute.js")
 const utilities = require("./utilities/")
+const session = require('express-session');
+const pool = require('./database/');
 
 
 // View Engine and Templates
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+
 
 /* ***********************
  * Routes
@@ -27,8 +56,9 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 // Index route
 // Inventory routes
-app.use("/inv", inventoryRoute)
 app.get('/', utilities.handleErrors(baseController.buildHome))
+app.use("/inv", inventoryRoute);
+app.use("/account", accountRoute);
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
